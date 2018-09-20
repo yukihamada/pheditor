79a5e6ef2df5ac71379ddf182a6be23748a0f0a5
Hamid <hamidreza.samak@gmail.com>
2018-09-20 09:55:25 +0430

    Disable self edit by default

--- pheditor.php
+++ pheditor.php
@@ -11,7 +11,7 @@
 define('PASSWORD', 'c7ad44cbad762a5da0a452f9e854fdc1e0e7a52a38015f23f3eab1d80b931dd472634dfac71cd34ebc35d16ab7fb8a90c81f975113d6c7538dc69dd8de9077ec');
 define('EDITABLE_FORMATS', 'txt,php,htm,html,js,css,tpl,xml,md');
 define('LOG_FILE', __DIR__ . DIRECTORY_SEPARATOR . '.phedlog');
-define('SHOW_PHP_SELF', true);
+define('SHOW_PHP_SELF', false);
 define('SHOW_HIDDEN_FILES', false);
 define('ACCESS_IP', '');
 
